5b3ed47d89bdeef5ab12a93f12423e8b2f57c8a4
h8k9 <h8k9@123.com>
2020-03-07 00:50:22 +0900

    Added visualization

--- scripts/image_synthesis.py
+++ scripts/image_synthesis.py
@@ -13,6 +13,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
 import reflectance_map as rm
+import utils
 
 def build_image(rmap, r,x,y):
 	"""
@@ -41,7 +42,7 @@ def draw_contours(x,y,z,ax,cm):
 	ax.contour(x, y, z, zdir='z', offset=z_min_draw, cmap=cm, levels = z_contours)
 	#return ax
  
-def reconstruct_normals(imgs, lights):
+def reconstruct_normals_3lights(imgs, lights):
 	"""
 	imgs: nxnx3 matriz with n the size of images stacked in depth
 	lights: 3x3 matrix of unitary incident light vectors
@@ -55,7 +56,20 @@ def reconstruct_normals(imgs, lights):
 			normals[i,j] = np.dot(linv,imgs[i,j])
 	return normals
 
-
+def reconstruct_normals_nlights(imgs, lights):
+	"""
+	imgs: nxnx3 matriz with n the size of images stacked in depth
+	lights: 3x3 matrix of unitary incident light vectors
+	"""
+	linv = np.linalg.pinv(lights)
+	#print(linv)
+	#linvs = np.full((16,16,3,3),linv)
+	normals = np.zeros((imgs.shape[0],imgs.shape[1],3))
+	#normals[:,:] = np.dot(linvs,imgs)
+	for i in range(imgs.shape[0]):
+		for j in range(imgs.shape[1]):
+			normals[i,j] = np.dot(linv,imgs[i,j])
+	return normals
 
 def normalize(v):
     norm = np.linalg.norm(v)
@@ -72,7 +86,7 @@ if __name__ == '__main__':
 	ax3 = fig.add_subplot(2,3,4)
 	ax4 = fig.add_subplot(2,3,5)
 	ax5 = fig.add_subplot(2,3,3, projection='3d')
-	ax6 = fig.add_subplot(2,3,6, projection='3d')
+	ax6 = fig.add_subplot(2,3,6)
 
 	r = 3
 
@@ -102,27 +116,31 @@ if __name__ == '__main__':
 	a,b,c,u,v,w = zip(*point_vectors)
 	ax1.quiver(a,b,c,u,v,w, pivot = "tail", color='r', arrow_length_ratio=0.3)
 
-
+	observer = np.array([0,0,-1])
 
 	# We need to syntethize images for 3 lights
 	light1 = np.array([0.7,0.3,-1])
 	light2 = np.array([-0.610,0.456,-1])
 	light3 = np.array([-0.090,-0.756,-1])
+	light4 = np.dot(utils.rotation_matrix(observer, np.pi/4),light1)
 
 	# get reflection maps for sphere
 	Ra1 = rm.get_Ra(normals,light1,1)
 	Ra2 = rm.get_Ra(normals,light2,1)
 	Ra3 = rm.get_Ra(normals,light3,1)
+	Ra4 = rm.get_Ra(normals,light4,1)
 
 	# generate images
 	im1 = build_image(Ra1,r,x,y)
 	im2 = build_image(Ra2,r,x,y)
 	im3 = build_image(Ra3,r,x,y)
+	im4 = build_image(Ra4,r,x,y)
 
 	# show images
 	ax2.imshow(im1)
 	ax3.imshow(im2)
 	ax4.imshow(im3)
+	ax6.imshow(im4)
 
 
 	# build reflection maps
@@ -134,10 +152,12 @@ if __name__ == '__main__':
 	Ra1 = rm.get_Ra(normals,light1,1)
 	Ra2 = rm.get_Ra(normals,light2,1)
 	Ra3 = rm.get_Ra(normals,light3,1)
+	Ra4 = rm.get_Ra(normals,light4,1)
 
 	s1 = ax5.plot_surface(p,q,Ra1, cmap = cm.viridis)
 	s2 = ax5.plot_surface(p,q,Ra2, cmap = cm.inferno)
 	s3 = ax5.plot_surface(p,q,Ra3, cmap = cm.magma)	
+	s4 = ax5.plot_surface(p,q,Ra4, cmap = cm.cividis)
 
 	# draw reflection map contours
 	fig2 = plt.figure(figsize=(20,20))
@@ -145,20 +165,29 @@ if __name__ == '__main__':
 	draw_contours(p,q,Ra1,fig2ax1, cm.Blues)
 	draw_contours(p,q,Ra2,fig2ax1, cm.Greens)
 	draw_contours(p,q,Ra3,fig2ax1, cm.Reds)
+	draw_contours(p,q,Ra4,fig2ax1, cm.RdPu)
 
-	# reconstruct surface using images and lights
+	# reconstruct normals using 3 images and lights
 	imgs = np.dstack((im1,im2,im3))
 	lights = np.vstack((normalize(light1), normalize(light2), normalize(light3)))
-	surf_normals = reconstruct_normals(imgs,lights)
+	surf_normals = reconstruct_normals_3lights(imgs,lights)
 	norm = np.linalg.norm(surf_normals,axis=2)
 	norm = np.dstack((norm, norm, norm))
 	normals_normalized = np.divide(surf_normals,norm)
 	point_vectors = np.dstack((starts,normals_normalized)).reshape(-1,6)
-	#print(point_vectors)
 	a,b,c,u,v,w = zip(*point_vectors)
 	ax1.quiver(a,b,c,u,v,w, pivot = "tail", color='g', arrow_length_ratio=0.3)	
 
-
+	# reconstruct normals using 4 images and lights
+	imgs = np.dstack((im1,im2,im3,im4))
+	lights = np.vstack((normalize(light1), normalize(light2), normalize(light3), normalize(light4)))
+	surf_normals = reconstruct_normals_nlights(imgs,lights)
+	norm = np.linalg.norm(surf_normals,axis=2)
+	norm = np.dstack((norm, norm, norm))
+	normals_normalized = np.divide(surf_normals,norm)
+	point_vectors = np.dstack((starts,normals_normalized)).reshape(-1,6)
+	a,b,c,u,v,w = zip(*point_vectors)
+	ax1.quiver(a,b,c,u,v,w, pivot = "tail", color='b', arrow_length_ratio=0.3)	
 
 	plt.tight_layout()
 	plt.show()
